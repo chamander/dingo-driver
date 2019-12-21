a85e218eecaf9e25256c67f83a9a4776d6041894
Gavan Chan <om.g.c@icloud.com>
2019-12-21 14:21:29 +1100

    Enable cancelling adding a session

--- Dingo Driver/Application/Dingo Driver/SessionListViewController.swift
+++ Dingo Driver/Application/Dingo Driver/SessionListViewController.swift
@@ -6,4 +6,8 @@ final class SessionListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    @IBAction private func cancelAddSession(_ unwindSegue: UIStoryboardSegue) {
+        // Segue to dismiss Add Session. Do nothing at the moment.
+    }
 }
