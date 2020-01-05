f98362a68fa6e085ded79dfd915a65965b59cefb
Gavan Chan <om.g.c@icloud.com>
2020-01-05 16:07:20 +1100

    Normalise values between 9º and 90º...
    ...represented by (π / 20) and (π / 2) respectively, in code.

--- Dingo Driver/Application/Remote Control/RemoteControlViewController.swift
+++ Dingo Driver/Application/Remote Control/RemoteControlViewController.swift
@@ -101,24 +101,79 @@ final class RemoteControlViewController: UIViewController {
 
             guard let self = self else { return }
 
-            let normalizedPitch: Double = motion.map { $0.attitude.pitch * 2 / .pi } ?? 0.0
-            let normalizedRoll: Double = motion.map { $0.attitude.roll * 2 / .pi } ?? 0.0
+            let throttle: Double = .adjustedValue(fromRawDeviceValue: motion?.attitude.roll ?? 0.0)
+            let steering: Double = .adjustedValue(fromRawDeviceValue: motion?.attitude.pitch ?? 0.0)
 
             DispatchQueue.main.async {
-                self.firstLabel?.text = "Pitch: \(String(describing: normalizedPitch))"
-                self.secondLabel?.text = "Roll: \(String(describing: normalizedRoll))"
+                self.firstLabel?.text = "Throttle: \(String(describing: throttle))"
+                self.secondLabel?.text = "Steering: \(String(describing: steering))"
             }
 
             self.session.publishData(
-                "throttle:\(normalizedPitch)".data(using: .utf8),
+                "throttle:\(throttle)".data(using: .utf8),
                 onTopic: connection.topic,
                 retain: false,
                 qos: .atLeastOnce)
             self.session.publishData(
-                "steering:\(normalizedRoll)".data(using: .utf8),
+                "steering:\(steering)".data(using: .utf8),
                 onTopic: connection.topic,
                 retain: false,
                 qos: .atLeastOnce)
+        }
+    }
+}
+
+private extension Double {
+
+    private static let minimumPositiveAngle: Double = .pi / 20
+    private static let maximumNegativeAngle: Double = -.minimumPositiveAngle
+
+    private static let maximumPositiveAngle: Double = .pi / 2
+    private static let minimumNegativeAngle: Double = -.maximumPositiveAngle
+
+    // Expecting that the device's raw value is in radians.
+    static func adjustedValue(fromRawDeviceValue value: Double) -> Double {
+
+        // Values are all between 0 and ±π.
+
+        if value > .zero {
+
+            // Provide (π / 20) of give, zero-ing values between 0c and (π / 20).
+            if value < .minimumPositiveAngle {
+                return .zero
+            }
+
+            // Maximise values at (π / 2). Anything larger returns maximum value.
+            if value > .maximumPositiveAngle {
+                return 1.0
+            }
+
+            // Value is between (π / 20) and (π / 2).
+            // Normalise between 0.0 and 1.0.
+            let scaled = value - .minimumPositiveAngle
+            let normalized = scaled / (.maximumPositiveAngle - .minimumPositiveAngle)
+            return normalized
+
+        } else if value < .zero {
+
+            // Provide (π / 20) of give, zero-ing values between -(π / 20) and 0c.
+            if value > .maximumNegativeAngle {
+                return .zero
+            }
+
+            // Maximise negative values at -(π / 2). Anything more negative returns minimum value.
+            if value < .minimumNegativeAngle {
+                return -1.0
+            }
+
+            // Value is between -(π / 2) and -(π / 20).
+            // Normalise between -1.0 and -0.0.
+            let scaled = value - .maximumNegativeAngle
+            let normalized = scaled / (.maximumNegativeAngle - .minimumNegativeAngle)
+            return normalized
+
+        } else {
+            return .zero
         }
     }
 }
